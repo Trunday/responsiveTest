a35d027e76b0804935612ecbc0dfafd38440876b
Trunday <musqa25@hotmail.com>
2020-10-14 19:16:13 +0300

    AnaSayfaTasarim

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
-import './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+  <div className="home-container">
+    <div className="container">
+      <h1>Öğretmenler için<br />başlangıç noktası!</h1>
+      <form className="form-inline">
+        <div className="form-group mb-2">
+          <input type="text" className="form-control" placeholder="E posta"/>
+        </div>
+        <div className="form-group mx-sm-3 mb-2">
+          <input type="password" className="form-control"placeholder="Şifre" />
+        </div>
+        <button type="submit" className="btn btn-primary mb-2">Giriş Yap</button>
+        <a href="#">Şifremi Unuttum</a>
+      </form>
+      <p>
+        Pariatur ex quis ex aute est ex proident veniam.<br/>
+        Üye olmak ister misin <br/>
+        Ea ea do mollit  amet non pariatur aute consectetur.<br/>
+        Ücretsiz üye olmak için <b>tıklayınız.</b>
+      </p>
+    </div>
   </div>
 )
 
